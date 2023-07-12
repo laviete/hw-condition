5f024d417eaf9ed309d42c9b9486cf5bb311b5c3
Alina <135998117+laviete@users.noreply.github.com>
2023-07-12 07:56:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,40 @@ if (userInputAge === '' || userInputAge === null || Number.isNaN(Number(userInpu
 } else if (userInputAge >= 100 && userInputAge <= 150) {
     console.log('respect');
 } 
+
+// Написати функцію Калькулятор, яка приймає аргументами два числа
+// и знак(рядок - +-*/) і повертає результат виконання дії або NaN, 
+// якщо неможливо виконати дію або не має такого знаку.
+/**
+ * 
+ * @param {number} num1 
+ * @param {number} num2 
+ * @param {string} operator 
+ * @returns {NaN|number}
+ */
+function calc(num1, num2, operator) {
+    if (num1 === '' || num1 === null || Number.isNaN(Number(num1)) || 
+    num2 === '' || num2 === null || Number.isNaN(Number(num2)) ||
+    operator !== "+" && operator !== "-" && operator !== "*" && operator !== "/") {
+        return NaN;
+    }
+    if (operator === '+') {
+        return num1 + num2;
+    } 
+    if (operator === '-') {
+        return num1 - num2;
+    }
+    if (operator === '*') {
+        return num1 * num2;
+    } 
+    if (operator === '/') {
+        return num2 === 0 ? NaN : num1 / num2;
+  }
+}
+console.log(calc(4, 3, '+'));  // 7
+console.log(calc(5, 2, '-'));  // 3
+console.log(calc(5, 2, '*'));  // 10
+console.log(calc(2, 2, '/'));  // 1
+console.log(calc('abc', 2, '*'));  // NaN
+console.log(calc(2, 0, '/'));  // NaN
+console.log(calc(2, 2, '%'));  // NaN - такого знаку у програмі не передбачено
